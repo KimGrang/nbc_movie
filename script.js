dccfff8f8141a30157ab895892534a28f2855b92
kimgrang <kimgrang1202@gmail.com>
2024-01-08 14:36:28 +0900

    prettier,reset.css

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTUzYzcxYzVkNDhiYTBlNGUxZDZkMjBkMjFiYTVhMyIsInN1YiI6IjY1OGUwZTc1ODQ0NDhlNjZkOWI1NjFhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kEbSQdSbsQxbG_KRm8rf8ZXXUjo-CSFzd7IgwsiU4GQ'
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTUzYzcxYzVkNDhiYTBlNGUxZDZkMjBkMjFiYTVhMyIsInN1YiI6IjY1OGUwZTc1ODQ0NDhlNjZkOWI1NjFhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kEbSQdSbsQxbG_KRm8rf8ZXXUjo-CSFzd7IgwsiU4GQ"
   }
 };
 
 // fetch API
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(data => {
+fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
+  .then((response) => response.json())
+  .then((data) => {
     displayMovies(data.results);
     idMovies(data.results);
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
 
 // card
 function displayMovies(movies) {
-  const movieList = document.getElementById('movieList');
-  const tempHtml = movies.map(movie => `
+  const movieList = document.getElementById("movieList");
+  const tempHtml = movies
+    .map(
+      (movie) => `
       <div class="card" style="width: 18rem;">
         <div class="card-header">
           <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" class="card-img-top" alt="...">
@@ -29,14 +32,16 @@ function displayMovies(movies) {
           <p class="card-text">Vote Average: ${movie.vote_average}</p>
         </div>
       </div>
-    `).join('');
+    `
+    )
+    .join("");
   movieList.innerHTML = tempHtml;
 }
 
 // 이미지 클릭 시 ID 알림
 function idMovies(movies) {
-  document.querySelectorAll('.card-img-top').forEach((img, index) => {
-    img.addEventListener('click', function () {
+  document.querySelectorAll(".card-img-top").forEach((img, index) => {
+    img.addEventListener("click", function () {
       const movieId = movies[index].id;
       alert(`ID: ${movieId}`);
     });
@@ -45,15 +50,15 @@ function idMovies(movies) {
 
 // 검색
 const searchMovies = function () {
-  const search = document.getElementById('search').value.toLowerCase();
-  const cards = document.querySelectorAll('.card');
-  cards.forEach(card => {
-    const title = card.querySelector('.card-title').innerText.toLowerCase();
+  const search = document.getElementById("search").value.toLowerCase();
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    const title = card.querySelector(".card-title").innerText.toLowerCase();
     if (title.includes(search)) {
-      card.style.display = 'block';
+      card.style.display = "block";
     } else {
-      card.style.display = 'none';
+      card.style.display = "none";
     }
   });
-}
-document.querySelector('.search_btn').addEventListener('click', searchMovies);
+};
+document.querySelector(".search_btn").addEventListener("click", searchMovies);
